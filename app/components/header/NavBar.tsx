629fd34e4c9aff682eb7c0e9ea619361997d9412
Valentaizar Hitsukaya <valentaizarstone@gmail.com>
2023-10-10 12:25:44 +0300

    Update

--- app/components/header/NavBar.tsx
+++ app/components/header/NavBar.tsx
@@ -9,7 +9,7 @@ const NavBar = () => {
      return (
 
 <nav className="hidden md:flex md:items-center md:justify-center md:gap-5">
-    <div className="fixed inset-x-0 top-0 z-30 mx-auto w-full max-w-screen-md border border-gray-100 bg-white/80 py-3 shadow backdrop-blur-lg md:top-6 md:rounded-3xl lg:max-w-screen-lg">
+    <div className="fixed inset-x-0 top-0 z-30 mx-auto w-full max-w-screen-md border border-gray-100 bg-white/80 py-2 shadow backdrop-blur-lg md:top-2 md:rounded-3xl lg:max-w-screen-lg">
     <div className="px-4">
         <div className="flex items-center justify-between">
             <div className="flex shrink-0">
