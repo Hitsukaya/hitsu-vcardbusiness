1355fce3fd03afaa957167775ce971e5ff3b1189
Valentaizar Hitsukaya <valentaizarstone@gmail.com>
2023-10-10 12:30:34 +0300

    Update

--- next-env.d.ts
+++ next-env.d.ts
@@ -3,3 +3,12 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
+
+
+POSTGRES_URL="postgres://default:i0hkfPCdq9Os@ep-wispy-meadow-65491361-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb"
+POSTGRES_PRISMA_URL="postgres://default:i0hkfPCdq9Os@ep-wispy-meadow-65491361-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb?pgbouncer=true&connect_timeout=15"
+POSTGRES_URL_NON_POOLING="postgres://default:i0hkfPCdq9Os@ep-wispy-meadow-65491361.us-east-1.postgres.vercel-storage.com:5432/verceldb"
+POSTGRES_USER="default"
+POSTGRES_HOST="ep-wispy-meadow-65491361-pooler.us-east-1.postgres.vercel-storage.com"
+POSTGRES_PASSWORD="i0hkfPCdq9Os"
+POSTGRES_DATABASE="verceldb"
