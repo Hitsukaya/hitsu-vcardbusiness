9bdcc3080a14b2e8cf22fb2acede84f03da303dc
Valentaizar Hitsukaya <valentaizarstone@gmail.com>
2023-10-10 12:03:06 +0300

    Update

--- app/components/header/NavBar.tsx
+++ app/components/header/NavBar.tsx
@@ -27,9 +27,9 @@ const NavBar = () => {
             </div>
             <div className="flex items-center justify-end gap-3">
                 <Link className="hidden items-center justify-center rounded-xl bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 transition-all duration-150 hover:bg-gray-50 sm:inline-flex"
-                    href="/login">Sign in</Link>
+                    href="#">Sign in</Link>
                 <Link className="inline-flex items-center justify-center rounded-xl bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm transition-all duration-150 hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
-                    href="/login">Get Qoute</Link>
+                    href="#">Get Qoute</Link>
             </div>
         </div>
     </div>
