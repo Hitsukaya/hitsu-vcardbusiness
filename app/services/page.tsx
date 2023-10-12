839ecea9833396cef641b64947b7b86fbfb587de
Hitsukaya Valentaizar <87619517+Hitsukaya@users.noreply.github.com>
2023-10-12 14:01:48 +0300

    add services page

---
+++ app/services/page.tsx
@@ -0,0 +1,16 @@
+// 'use client'
+import Link from "next/link";
+import Image from 'next/image'
+import React from "react"
+
+
+const Services = () => {
+    return ( 
+
+        <h1></h1>
+
+
+     );
+}
+ 
+export default Services;
